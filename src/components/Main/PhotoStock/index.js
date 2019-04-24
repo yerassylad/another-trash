78fd429bc5b6b76f9b13b10ee736ce3b4490d755
YerassylAitkazy <yerassyl.ad@gmail.com>
2019-04-24 15:13:47 +0600

    photoStock container

--- src/components/Main/PhotoStock/index.js
+++ src/components/Main/PhotoStock/index.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import MobilePhotoStock from "./MobilePhotoStock";
 import RegularPhotoStock from "./RegularPhotoStock";
 import spreadArray from "../../../libs/spreadArray";
 
 export class PhotoStock extends Component {
   render() {
-    const { images } = this.props;
-    const columns = 3;
+    const { images, deviceType } = this.props;
+    const columns = deviceType;
     if (columns === 1) return <MobilePhotoStock photos={images} />;
     const columnedPhotos = spreadArray(images, columns);
     return <RegularPhotoStock columnedPhotos={columnedPhotos} />;
   }
 }
 
-export default PhotoStock;
+const mapStateToProps = state => ({
+  deviceType: state.core.deviceType
+});
+
+export default connect(mapStateToProps)(PhotoStock);
