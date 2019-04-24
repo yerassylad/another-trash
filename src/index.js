7b500efa0632825e9414b9a0b5e90b17e3286450
YerassylAitkazy <yerassyl.ad@gmail.com>
2019-04-24 14:36:37 +0600

    redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import App from "./App";
+import rootReducer from "./reducers";
+
 import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const enhancer = compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+);
+
+const store = createStore(rootReducer, {}, enhancer);
+
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootEl
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import core from "./Core";
+
+export default combineReducers({
+  core
+});
