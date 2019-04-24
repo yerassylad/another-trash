9450fb3c6903bad8fa705bd3a43c709cfb076f7c
YerassylAitkazy <yerassyl.ad@gmail.com>
2019-04-24 15:09:41 +0600

    core app states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import debounce from "./libs/debounce";
+import determineScreenSizes from "./libs/determineScreenSizes";
+import updateDeviceType from "./actions/Core/updateDeviceType";
+import updateScreenSizes from "./actions/Core/updateScreenSizes";
 import ModalSwitch from "./ModalSwitch";
 
-const App = () => {
-  return (
-    <Router>
-      <Route component={ModalSwitch} />
-    </Router>
-  );
-};
+export class App extends Component {
+  debouncedScreenSizesUpdater = debounce(() => {
+    const { updateScreenSizes } = this.props;
+    const screenSizes = determineScreenSizes();
+    updateScreenSizes(screenSizes);
+  }, 150);
 
-export default App;
+  deviceTypeUpdater = () => {
+    const { updateDeviceType } = this.props;
+    const deviceType = this.determineDeviceType();
+    updateDeviceType(deviceType);
+  };
+
+  determineDeviceType = () => {
+    const { screenWidth } = this.props;
+    if (screenWidth > 991) {
+      return 3;
+    } else if (screenWidth <= 991 && screenWidth > 479) {
+      return 2;
+    } else if (screenWidth <= 479) {
+      return 1;
+    }
+  };
+
+  componentWillMount = () => {
+    window.addEventListener("resize", this.debouncedScreenSizesUpdater);
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener("resize", this.debouncedScreenSizesUpdater);
+  };
+
+  componentDidMount = () => {
+    this.debouncedScreenSizesUpdater();
+  };
+
+  componentDidUpdate = prevProps => {
+    const { screenWidth } = this.props;
+    if (screenWidth !== prevProps.screenWidth) {
+      this.deviceTypeUpdater();
+    }
+  };
+
+  render() {
+    return (
+      <Router>
+        <Route component={ModalSwitch} />
+      </Router>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  screenWidth: state.core.screenSizes.viewPortWidth
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    updateScreenSizes,
+    updateDeviceType
+  }
+)(App);
