89a83457edc67ad0484d277351222807f0577953
YerassylAitkazy <yerassyl.ad@gmail.com>
2019-05-02 14:52:17 +0600

    deploy netlify

--- src/api.js
+++ src/api.js
@@ -5,8 +5,10 @@ const unsplash = axios.create({
   headers: {
     "Accept-Version": "v1",
     Authorization:
-      "Client-ID 521b026b88fd40f46026aa9432cd3d92dc050658a54e78c116fa60b624ebcfc8"
+      "Client-ID 7ae6287041b27d388038d2ec48a2ae344b50c30def017efa2e2fcecc9625c270"
   }
 });
 
 export default unsplash;
+
+// 521b026b88fd40f46026aa9432cd3d92dc050658a54e78c116fa60b624ebcfc8
