e989f189250f82c614514dddcf2ed2637d40810d
YerassylAitkazy <yerassyl.ad@gmail.com>
2019-05-02 12:23:16 +0600

    wrapper

--- src/components/Main/PhotoStock/RegularPhotoStock.js
+++ src/components/Main/PhotoStock/RegularPhotoStock.js
@@ -5,6 +5,7 @@ import { withRouter } from "react-router-dom";
 import ImageDimmer from "../ImageDimmer";
 import penultImage from "../../../HOCs/penultImage";
 import incrementPage from "../../../actions/Pohotos/incrementPage";
+import Wrapper from "../Wrapper";
 
 const PenultImage = penultImage(Image);
 
@@ -19,43 +20,45 @@ const RegularPhotoStock = props => {
   };
 
   return (
-    <Grid>
-      <Grid.Row columns={columns}>
-        {columnedPhotos.map((column, columnIndex) => {
-          const columnPenultPhotoIndex = column.length - 2;
-          return (
-            <Grid.Column key={columnIndex}>
-              {column.map((photo, photoIndex) => {
-                if (columnPenultPhotoIndex === photoIndex) {
+    <Wrapper>
+      <Grid>
+        <Grid.Row columns={columns}>
+          {columnedPhotos.map((column, columnIndex) => {
+            const columnPenultPhotoIndex = column.length - 2;
+            return (
+              <Grid.Column key={columnIndex}>
+                {column.map((photo, photoIndex) => {
+                  if (columnPenultPhotoIndex === photoIndex) {
+                    return (
+                      <ImageDimmer
+                        key={photo.id}
+                        handleGoToPhoto={handleGoToPhotoFn(photo.id)}
+                        user={photo.user}
+                      >
+                        <PenultImage
+                          onImageVisible={incrementPage}
+                          src={photo.urls.small}
+                          fluid
+                        />
+                      </ImageDimmer>
+                    );
+                  }
                   return (
                     <ImageDimmer
                       key={photo.id}
                       handleGoToPhoto={handleGoToPhotoFn(photo.id)}
                       user={photo.user}
                     >
-                      <PenultImage
-                        onImageVisible={incrementPage}
-                        src={photo.urls.small}
-                        fluid
-                      />
+                      <Image fluid src={photo.urls.small} />
                     </ImageDimmer>
                   );
-                }
-                return (
-                  <ImageDimmer
-                    key={photo.id}
-                    handleGoToPhoto={handleGoToPhotoFn(photo.id)}
-                    user={photo.user}
-                  >
-                    <Image fluid src={photo.urls.small} />
-                  </ImageDimmer>
-                );
-              })}
-            </Grid.Column>
-          );
-        })}
-      </Grid.Row>
-    </Grid>
+                })}
+              </Grid.Column>
+            );
+          })}
+        </Grid.Row>
+      </Grid>
+    </Wrapper>
   );
 };
 
---
+++ src/components/Main/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled("div")`
+  max-width: 1320px;
+  padding-left: 12px;
+  padding-right: 12px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export default Wrapper;
